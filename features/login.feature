2fb548984ad4921177dd767b5790c5efeb70a7b4
Gayatri123123 <gayatrishekharredyy@gmail.com>
2023-12-28 14:54:24 +0530

    changed some scenarios

--- features/login.feature
+++ features/login.feature
@@ -3,13 +3,22 @@ Feature: Login functionality
   Background: Common steps
     Given I lauched chrome browser
     When I open orange HRM Homepage
-    And Enter username "Admin" and password "admin123"
-    And clicks on Login button
 
-  @search
+  @Dashboard
   Scenario: Login with valid credentials
-    Then user should get logged in to dashboard page
-  @search1
-  Scenario: Login with valid credentials
+    When Enter username "Admin" and password "admin123"
+    When clicks on Login button
+    Then user should get logged into dashboard page
+
+  @Adminpage
+  Scenario: Login with valid credentials for admin page
+    When Enter username "Admin" and password "admin123"
+    When clicks on Login button
     When click on Admin button
     Then highlight the System users
+
+  @Invalid
+  Scenario:Login with Invalid credentials
+    When Enter Invalid username "abc" and password "abc123"
+    And clicks on Login button
+    Then user should get proper error message
--- features/steps/login.py
+++ features/steps/login.py
@@ -5,40 +5,61 @@ from selenium import webdriver
 
 
 @given('I lauched chrome browser')
-def lanch_browser(context):
+def launch_browser(context):
     context.driver = webdriver.Chrome()
 
 @when(u'I open orange HRM Homepage')
-def step_impl(context):
+def launch_orange_hrm_page(context):
     context.driver.get("https://opensource-demo.orangehrmlive.com/")
     context.driver.maximize_window()
     time.sleep(2)
 
-@when(u'Enter username "{username}" and password "{password}"')
-def step_impl(context,username,password):
-    context.driver.find_element(By.XPATH,"//*[@name='username']").send_keys(username)
-    context.driver.find_element(By.XPATH,"//*[@name='password']").send_keys(password)
+@when(u'Enter username "Admin" and password "admin123"')
+def enter_user_credentail(context,):
+    context.driver.find_element(By.XPATH,"//*[@name='username']").send_keys("Admin")
+    context.driver.find_element(By.XPATH,"//*[@name='password']").send_keys("admin123")
 
 @when(u'clicks on Login button')
-def step_impl(context):
+def click_on_login_btn(context):
    context.driver.find_element(By.XPATH,"//*[@type='submit']").click()
 
-@then(u'user should get logged in to dashboard page')
-def step_impl(context):
+@then(u'user should get logged into dashboard page')
+def verifying_dashboard_page(context):
     time.sleep(5)
-    text = context.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6').text
-    assert text == 'Dashboard'
+    text = context.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6')
+    context.driver.execute_script("arguments[0].style.color='yellow'",text)
+    #assert text == 'Dashboard'
+    time.sleep(3)
     context.driver.close()
 
 @when(u'click on Admin button')
-def step_impl(context):
+def click_on_admin_btn(context):
     time.sleep(3)
     context.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a').click()
 
 
 @then(u'highlight the System users')
-def step_impl(context):
+def highlight_system_user(context):
     time.sleep(3)
     ele=context.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[1]/h5')
+    context.driver.execute_script("arguments[0].style.color='yellow'",ele)
+    time.sleep(5)
+'''
+
+@when(u'Enter Invalid username "{username}" and password "{password}"')
+def enter_user_Invalid_credentail(context,username,password):
+    context.driver.find_element(By.XPATH,"//*[@name='username']").send_keys(username)
+    context.driver.find_element(By.XPATH,"//*[@name='password']").send_keys(password)
+'''
+
+@when(u'Enter Invalid username "abc" and password "abc123"')
+def enter_user_Invalid_credentail(context):
+    context.driver.find_element(By.XPATH,"//*[@name='username']").send_keys("abc")
+    context.driver.find_element(By.XPATH,"//*[@name='password']").send_keys("abc123")
+
+@then(u'user should get proper error message')
+def verfying_invalid_credentials(context):
+    time.sleep(2)
+    ele=context.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]')
     context.driver.execute_script("arguments[0].style.color='yellow'",ele)
     time.sleep(5)
