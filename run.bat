0f57c43f75a72fcfa0cb8f1af7f6bd62004c5bc2
Gayatri123123 <152398636+Gayatri123123@users.noreply.github.com>
2024-01-03 15:02:38 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 call ./Scripts/activate.bat
 call behave -f allure_behave.formatter:AllureFormatter -o Reports\allure_result
-call allure generate Reports\allure_result -o Reports\allure_report --clean
+::call allure generate Reports\allure_result -o Reports\allure_report --clean
